a26a46eb4c06352d788667451ef8b0135001e2c4
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-03-19 12:50:54 +0000

    v1.1

---
+++ python/ap_19,03,24.py
@@ -0,0 +1,3 @@
+def nombre_suivant(s):
+    '''Renvoie le nombre suivant de celui represente par s en appliquant le procede de lecture''' # ceci est du doc string, donc si tu fait print(help(print)), cette phrase va etre emise.
+    resultat = ''
