8360eeec5ef7a12ccbdee5025f663443259471f8
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-03-19 13:14:41 +0000

    v1.1.1

--- python/ap_19,03,24.py
+++ python/ap_19,03,24.py
@@ -1,3 +1,18 @@
 def nombre_suivant(s):
-    '''Renvoie le nombre suivant de celui represente par s en appliquant le procede de lecture''' # ceci est du doc string, donc si tu fait print(help(print)), cette phrase va etre emise.
+    '''Renvoie le nombre suivant de celui represente par s en appliquant le procede de lecture, je suis le plus fort''' 
+    # ceci est du doc string, donc si tu fait print(help(la fonction)), cette phrase va etre emise, python a ca pour ses fonctions.
     resultat = ''
+    chiffre = s[0] # recupere la valeur dans la liste s a l'index 0.
+    compte = 1
+    for i in range(1, len(s)): # pour chaque element dans la liste s
+        if s[i] == chiffre:
+            compte = compte + 1
+        else:
+            resultat += str(compte) + chiffre
+            chiffre = compte
+            ...
+    lecture_chiffre = str(compte) + chiffre
+    resultat += lecture_chiffre
+    return resultat        
+
+print(help(nombre_suivant))
