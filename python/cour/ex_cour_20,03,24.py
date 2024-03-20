62b4a9dabff2e0f9affd6fb795093310f85094f5
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-03-20 09:12:54 +0000

    v1.2

--- python/cour/ex_cour_20,03,24.py
+++ python/cour/ex_cour_20,03,24.py
@@ -1,9 +1,12 @@
 def enumere(tab):
     d = {}
     for i in range (len(tab)):
-        d[tab[i]] = [i]
-    print(d)
+        if tab[i] in d: # if the value is already in the dictionary
+            d[tab[i]].append(i) # append si la valeur est deja dans le dictionnaire.
+        else:
+           d[tab[i]] = [i]
+    return d # ALWAYS use return instead of print.
 
 assert enumere([1, 2, 3]) == {1: [0], 2: [1], 3: [2]}
-assert enumere([1, 1, 2, 3, 2, 1]) == {1: [0, 1, 5], 2: [2, 4], 3: [3]}
 assert enumere([]) == {}
+assert enumere([1, 1, 2, 3, 2, 1]) == {1: [0, 1, 5], 2: [2, 4], 3: [3]}
