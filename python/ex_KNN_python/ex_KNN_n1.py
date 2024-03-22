68a412ceddb56eb32e66f0fc21e201e0700d46b2
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-03-22 15:42:23 +0000

    reorganisation

--- python/ex_KNN_python/ex_KNN_n1.py
+++ python/ex_KNN_python/ex_KNN_n1.py
@@ -1,7 +1,7 @@
 # Données d'entraînement
-import numpy as np
-donnees_entrainement = np.array([[2, 4], [1, 3], [2, 3], [3, 2], [2, 1], [5, 6], [4, 5], [4, 6], [6, 6], [5, 4]])
-etiquettes_entrainement = np.array(['blue', 'blue', 'blue', 'blue', 'blue', 'orange', 'orange', 'orange', 'orange', 'orange']) 
+
+donnees_entrainement = ([[2, 4], [1, 3], [2, 3], [3, 2], [2, 1], [5, 6], [4, 5], [4, 6], [6, 6], [5, 4]])
+etiquettes_entrainement = (['blue', 'blue', 'blue', 'blue', 'blue', 'orange', 'orange', 'orange', 'orange', 'orange']) 
 
 def_distance = {}
 for i in range(len(donnees_entrainement)):
