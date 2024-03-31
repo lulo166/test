f09e98f74bf01b5535ac413cb06b902dea2de730
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-03-31 17:25:35 +0100

    v1

--- python/test2.py
+++
@@ -1,5 +0,0 @@
-def distanceeuclidienne(liste1, liste2):
-   distance = {} # la distance est d'abord nulle
-   for i in range(len(liste1)): # pour chaque element de la liste (chaque pixel de l'image)
-     distance += (liste1[i] - liste2[i]) ** 2 # a la distance on ajoute la difference au carre entre les deux pixels
-   return distance ** 0.5 # on retourne la racine carree de la distance
