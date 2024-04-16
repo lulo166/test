06032d538ab57b95dbb77d79f944b8d1910e3b2d
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-04-16 13:05:25 +0100

    v1

---
+++ python/AP/ex_16,04,24.py
@@ -0,0 +1,23 @@
+def echange(tab, i, j):
+    '''Echange les éléments d'indice i et j dans le tableau tab.'''
+    temp = tab[i] 
+    tab[i] = tab[j] 
+    tab[j] = temp
+
+def tri_selection(tab):
+    '''Trie le tableau tab dans l'ordre croissant
+    par la méthode du tri par sélection.'''
+    N = len(tab)
+    for k in range(...): 
+        imin = ... 
+        for i in range(..., N): 
+            if tab[i] < ...: 
+                imin = i
+        echange(tab, ..., ...)
+
+
+def tri_min_max(tab):
+    for i in range (len(tab)):
+        for j in range(i, len(tab)):
+            if tab[i] >= tab[j]:
+                
