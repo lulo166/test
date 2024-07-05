bd48cf5c8a3d42065d489edae96e5e3213ba159b
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-07-05 15:47:05 +0000

    v2

--- python/Maison/ex05,07,24.py
+++ python/Maison/ex05,07,24.py
@@ -21,3 +21,28 @@ def notes_triees(effectif_notes):
 
 assert notes_triees([0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
+
+def dec_to_bin(nb_dec):
+    q, r = nb_dec // 2, nb_dec % 2
+    if q == 0:
+        return str(r) 
+    else:
+        return dec_to_bin(q) + str(r)
+
+assert dec_to_bin(10) == '1010'
+
+def bin_to_dec(nb_bin):
+    if len(nb_bin) == 1:
+        if nb_bin == '0':
+            return 0
+        else:
+            return 1
+    else:
+        if nb_bin[-1] == '0':
+            bit_droit = 0
+        else:
+            bit_droit = 1
+        return 2 * bin_to_dec(nb_bin[:-1]) + bit_droit
+
+assert bin_to_dec('1010') == 10
