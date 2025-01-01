846ea70b887a4001af0907b4871eaad21dd2b74a
lulo166 <146172440+lulo166@users.noreply.github.com>
2025-01-01 19:15:47 +0000

    ex 6

--- python/concour_generel.py
+++ python/concour_generel.py
@@ -1,3 +1,4 @@
+1.1
 Q1 = 'Done'
 def verif_etage_critique(c):
     if lacher(c-1) == False:
@@ -46,6 +47,37 @@ def critique_dichotomie(n):
 #car la division reduit les deux valeurs vers un seul même valeur
 #et quand elles deviennent égales, la boucle s'arrête.
 
-Q4 = 'a faire'
+Q4 = 'fait'
+#On peut faire des lancers en prenant un intervale de p, 
+#car si on divise p**2 par p, on se retrouve avec p.
+#Puis dès que la première balle se casse, on sait dans quel intervale
+#il faut lancer la deuxième balle. On lance la deuxième balle jusqu'a ce
+#que la deuxième balle se casse, et on a le résultat.
+
+#En tout on lance les deux balles de cette manière.
+#On lance la première balle un nombre <= p fois, 
+#Puis notre deuxième balle un nombre <= p fois,
+#ce qui va être un nombre de lancer plus petit ou égal à 2p.
+
+
+
+1.2
+Q5 = 'fait'
+#1) On dispose de r-1 oeufs, et il reste i-1 étages à tester.
+
+#2) On dispose de r-1 oeufs, et il reste n-i étages à tester.
+
+#3) Non car si l'oeuf ne s'est pas cassé à l'étage i, 
+# il ne se cassera pas à l'étage i-1 car c'est un étage plus proche 
+# du sol que l'étage i, même chose pour i-2, et etc...
+# On dispose de r-1 oeufs, et il reste n-i étages à tester, 
+# donc les étages i+1, i+2, etc...
+
+Q6 = 'a faire'
+#
+
+
+
+
 
 
